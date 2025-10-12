37611b8f30d4df3de6d0c16091ce024ead2bfa4d
GayathriDeshaveni <gayathri.deshaveni3@gmail.com>
2025-10-12 06:04:51 -0400

    after completion of sign-up

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,11 +1,15 @@
-import LoginPanel from "./components/Login/Login"
 import { Routes, Route } from "react-router-dom";
+import LoginPanel from "./components/Login/Login";
+import RegisterPanel from "./components/Register/Register"; // import your register component
 
 function App() {
   return (
     <Routes>
       <Route path="/login" element={<LoginPanel />} />
+      <Route path="/register" element={<RegisterPanel />} /> {/* New route */}
+      <Route path="/" element={<LoginPanel />} /> {/* Optional: default to login */}
     </Routes>
   );
 }
+
 export default App;
